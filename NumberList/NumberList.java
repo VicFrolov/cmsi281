0d2b809919036c015ef8dde263a60a3654e0fc4a
Victor Frolov <vicfrolov@gmail.com>
2015-10-09 17:56:46 -0700

    Added all the methods to implement and extended interface

--- NumberList/NumberList.java
+++ NumberList/NumberList.java
@@ -1,5 +1,105 @@
-public class NumberList {
+public class NumberList implements java.util.Collection {
+
+	public NumberList() {
+
+	}
+
+	public NumberList(java.lang.Long[] l) {
+
+	}
+
+	public boolean add(java.lang.Object obj) {
+		return false;
+	}
+
+	public boolean addAll(java.util.Collection c) {
+		return false;
+
+	}
+
+	public boolean contains(java.lang.Object obj) {
+		return false;
+
+	}
+
+	public boolean containsAll(java.util.Collection c) {
+		return false;
+
+	}
+
+	public boolean equals(java.lang.Object obj) {
+		return false;
+
+	}
+
+	public int hashCode() {
+		return 0;
+	}
+
+	public boolean isEmpty() {
+		return false;
+
+	}
+
+	public java.util.Iterator iterator() {
+		return null;
+	}
+
+	public boolean remove(java.lang.Object obj) {
+		return false;
+
+	}
+
+	public boolean removeAll(java.util.Collection c) {
+		return false;
+
+	}
+
+	public boolean retainAll(java.util.Collection c) {
+		return false;
+
+	}
+
+	public int sizeIncludingDuplicates() {
+		return 0;
+	}
+
+
+	public NumberList[] toArray() {
+		return null;
+	}
+
+	// Should be unsupported 
+	public NumberList[] toArray(java.lang.Object[] obj) {
+		return null;
+	}
+
+	public int size() {
+		return 0;
+
+	}
+
+
+
+	public void clear() {
+
+	}
+
+	public int count(java.lang.Object obj) {
+		return 0;
+
+	}
 	
+	public java.lang.String toString() {
+		return null;
+
+	}
+
+	public static NumberList fromArray(long[] l) {
+		return null;
+
+	}
+
 	public static void main(String[] args) {
 
 	}
