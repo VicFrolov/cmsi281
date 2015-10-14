af1c5be5d2d3bb909a87cc353cf86ea1972b28a3
Victor Frolov <vicfrolov@gmail.com>
2015-10-13 18:56:23 -0700

    added test cases for constructors and adder methods

--- NumberList/NumberListTestHarness.java
+++ NumberList/NumberListTestHarness.java
@@ -8,6 +8,7 @@ public class NumberListTestHarness {
         successes = 0;
 
         test_Me();
+        adder();
      
 
         System.out.println(successes + "/" + attempts + " tests passed.");
@@ -21,20 +22,67 @@ public class NumberListTestHarness {
     }
 
     private static void test_Me() {
-        System.out.println("Testing Stuff...");
-    
-        
-        // try {
-        //     displaySuccessIfTrue();
-        // } catch(Exception e) {
-        //     displaySuccessIfTrue(false);
-        // }
-        // try {
-        //     displaySuccessIfTrue();
-        // } catch(Exception e) {
-        //     displaySuccessIfTrue(false);
-        // }        
+        System.out.println("Testing constructors...");
+        NumberList nl = new NumberList();
+        NumberList nl2 = new NumberList(new Long[] {new Long(1), new Long(2), new Long(3)});
+        NumberList nl3 = new NumberList(new Long[] {new Long(1)});
 
+        try {
+            displaySuccessIfTrue(nl.size() == 0);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        try {
+            displaySuccessIfTrue(nl2.size() == 3);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }        
+        try {
+            displaySuccessIfTrue(nl.isEmpty() == true);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        try {
+            displaySuccessIfTrue(nl2.isEmpty() == false);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        // try {
+        //     displaySuccessIfTrue(NumberList nl4 = new NumberList(5) == false);
+        // } catch(Exception e) {
+        //     displaySuccessIfTrue(false);
+        // }         
         
+    }
+
+    private static void adder() {
+        System.out.println("Testing adding objects...");
+        NumberList nl = new NumberList();
+        NumberList nl2 = new NumberList(new Long[] {new Long(1), new Long(2), new Long(3)});
+        NumberList nl3 = new NumberList(new Long[] {new Long(1)});
+        nl.add(new Long(2));
+        nl3.add(new Long(2));
+        nl3.add(new Long(3));
+
+        try {
+            displaySuccessIfTrue(nl.size() == 1);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        try {
+            displaySuccessIfTrue(nl3.size() == 3);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }        
+        try {
+            displaySuccessIfTrue(nl.isEmpty() == false);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+        try {
+            displaySuccessIfTrue(nl2.isEmpty() == false);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
     }
 }
