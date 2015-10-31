4c2b1ef4017439aac37d7839f7e0bb89bcf948a7
Victor Frolov <vicfrolov@gmail.com>
2015-10-31 00:26:07 -0700

    initial LinkedDeque commit

---
+++ LinkedDeque/LinkedDeque.java
@@ -0,0 +1,57 @@
+public class LinkedDeque {
+	int size;
+	
+	public LinkedDeque() {
+        throw new UnsupportedOperationException();
+
+	}
+
+	public void insertLeft(Object o) {
+        throw new UnsupportedOperationException();
+
+	}
+
+	public void insertRight(Object o) {
+        throw new UnsupportedOperationException();
+
+	}
+
+	public void deleteLeft() {
+        throw new UnsupportedOperationException();
+
+	}
+
+	public void deleteRight() {
+        throw new UnsupportedOperationException();
+
+	}
+
+	public Object left() {
+		// returns the left element without modifiying the deque
+		throw new UnsupportedOperationException();
+
+	}
+
+	public Object right() {
+		// returns the right element without modifiying the deque
+		throw new UnsupportedOperationException();
+
+	}
+
+	public int size() {
+        throw new UnsupportedOperationException();
+
+	}
+
+	public String toString(){
+		// returns [obj][obj]...[obj]
+        throw new UnsupportedOperationException();
+
+	} 
+
+	public static void main(String[] args) {
+		// runs a comprehensive set of unit tests
+		
+	}
+
+}
