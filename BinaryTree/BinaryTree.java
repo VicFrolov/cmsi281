df02fb3cd7f364de5b8a4597a6a486608531f578
Victor Frolov <vicfrolov@gmail.com>
2015-11-28 18:09:43 -0800

    initial commit for BinaryTree

---
+++ BinaryTree/BinaryTree.java
@@ -0,0 +1,50 @@
+import java.util.Iterator;
+
+public class BinaryTree implements java.lang.Iterable {
+	private int size;
+	private Object[] binaryTree;
+
+	public BinaryTree() {
+
+	}
+
+	public BinaryTree(Object obj) {
+
+	}
+
+	public boolean contains(Object obj) {
+		return false;
+	}
+
+	public boolean similar(Object obj) {
+		return false;
+	}
+
+	public boolean equals(Object obj) {
+		return false;
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+	public int size() {
+		return -1;
+	}
+
+	public int hashCode() {
+		return -1;
+	}
+	
+	public Iterator iterator()  {
+        throw new UnsupportedOperationException();
+	}	
+
+	public Iterator inOrder() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
