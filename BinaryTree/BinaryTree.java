7cc0cf23bfc346d24251da5b60be9a5b400772ee
Victor Frolov <vicfrolov@gmail.com>
2015-12-01 23:47:46 -0800

    added more basic templates, and implemented the majority of Node class, for getter and setter methods including constructor

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,15 +1,15 @@
-import java.util.Iterator;
-
-public class BinaryTree implements java.lang.Iterable {
+public class BinaryTree implements Iterable {
 	private int size;
+	private Node root;
 	private Object[] binaryTree;
 
 	public BinaryTree() {
+		this.size = 0;
 
 	}
 
 	public BinaryTree(Object obj) {
-
+		throw new UnsupportedOperationException();
 	}
 
 	public boolean contains(Object obj) {
@@ -25,11 +25,11 @@ public class BinaryTree implements java.lang.Iterable {
 	}
 
 	public boolean isEmpty() {
-		return false;
+		return this.size == 0 ? true : false;
 	}
 
 	public int size() {
-		return -1;
+		return this.size;
 	}
 
 	public int hashCode() {
@@ -45,6 +45,71 @@ public class BinaryTree implements java.lang.Iterable {
 	}
 
 	public static void main(String[] args) {
-		
+
 	}
 }
+
+class Iterator implements java.util.Iterator {
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Node next() {
+		throw new UnsupportedOperationException();
+
+	}
+
+	public boolean hasNext() {
+		throw new UnsupportedOperationException();
+
+	}
+}
+
+class Node {
+	private Object data;
+	private Node leftSon;
+	private Node rightSon;
+	private Node rightBrother;
+	private Node parent;
+
+	Node(Object data, Node parent) {
+		this.data = data;
+		this.parent = parent;		
+		this.leftSon = null;
+		this.rightSon = null;
+		this.rightBrother = null;
+	}
+
+	public void setLeftSon(Node n) {
+		this.leftSon = n;
+	}
+
+	public void setRightSon(Node n) {
+		this.rightSon = n;
+	}
+	
+	public void setRightBrother(Node n) {
+		this.rightBrother = n;
+	}
+
+	public void setParent(Node n) {
+		this.parent = n;
+	}
+
+	public Node getLeftSon() {
+		return this.leftSon;
+	}
+
+	public Node getRightSon() {
+		return this.rightSon;
+	}
+
+	public Node getRightBrother() {
+		return this.rightBrother;
+	}
+
+	public Node getParent() {
+		return this.parent;
+	}	
+}
+
