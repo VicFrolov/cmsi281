0b58d79dd1c4e791da93e80c72efb71237d60bb7
Victor Frolov <vicfrolov@gmail.com>
2015-12-09 20:56:27 -0800

    implemented clear hyphen and set delimteres method that returns an array

--- WordFrequencies/WordFrequencies.java
+++ WordFrequencies/WordFrequencies.java
@@ -1,7 +1,27 @@
-public class WordFrequencies extends java.lang.Object {
-	
+import java.util.HashMap;
+
+public class WordFrequencies {
+	HashMap map;
+	HashSet set;
+
+	public WordFrequencies() {
+
+	}
+
+	public static String[] clearHyphensAndSetDelimeters(String s) {
+		return s.replaceAll("-", "").replaceAll("[^A-Za-z0-9]", " ").split(" ");
+	}
+
 
 	public static void main(String[] args) {
 
+
+		String lol = "They're coming for y-ou!! ?Man I hope not!!.      l   l       @#1";
+		String[] cleanString = clearHyphensAndSetDelimeters(lol);
+
+		for  (String si : cleanString) {
+			System.out.print("length : " + si.length() + " String: ");
+			System.out.println(si);
+		}
 	}
 }
