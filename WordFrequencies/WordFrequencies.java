389e6021d46e43b4beab740d1c5cd6c6338ae862
Victor Frolov <vicfrolov@gmail.com>
2015-12-09 23:19:13 -0800

    implemented a checker to make sure if an args is provided it is a valid one, and that there is only one args. implemented conversion in main from input to the appropriate hashmap with uppercase or non-case sensitive letters

--- WordFrequencies/WordFrequencies.java
+++ WordFrequencies/WordFrequencies.java
@@ -1,27 +1,82 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class WordFrequencies {
-	HashMap map;
-	HashSet set;
 
-	public WordFrequencies() {
+    private static String[] wordFrequencyArray(String s) {
+        return s.replaceAll("-", "").replaceAll("[^A-Za-z0-9]", " ").toUpperCase().split(" ");
+    }
 
-	}
+    private static String[] wordFrequencyArrayWithoutUppercase(String s) {
+        return s.replaceAll("-", "").replaceAll("[^A-Za-z0-9]", " ").split(" ");
+    }
 
-	public static String[] clearHyphensAndSetDelimeters(String s) {
-		return s.replaceAll("-", "").replaceAll("[^A-Za-z0-9]", " ").split(" ");
-	}
+    private static HashMap<String, Integer> turnIntoHashMapAndSet(String[] inputArray) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>(inputArray.length);
 
+        for (String s : inputArray) {
+            if (s.length() > 0) {
+                if (map.get(s) == null) {
+                    map.put(s, 1);
+                } else {
+                    map.put(s, (map.get(s) + 1));
+                }
+            }
+        }
+        return map;
+    }
+    public static String invalidInput() {
+        return "Please either only pipe in a file, or use '-s', '-c' or a mix of them with only one hyphen";
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        String fileInput = "";
+        String[] userOptions = new String[]{"-s", "-c", "-sc", "-cs"};
+        boolean caseSensitive = false;
+        String userConstraint = "";
+        String[] cleanStringArray;
+        Scanner scanner = new Scanner(System.in);
 
+        if (args.length > 1) {
+            System.out.println(invalidInput());
+            return;
+        } else {
+            userConstraint = args[0];
+            boolean match = false;
+            
+            for (String s : userOptions) {                
+                if (userConstraint.equals(s)) {
+                    match = true;
+                }
+            }
+            if(!match) {
+                System.out.println(invalidInput());
+                return;
+            }
+        }
 
-		String lol = "They're coming for y-ou!! ?Man I hope not!!.      l   l       @#1";
-		String[] cleanString = clearHyphensAndSetDelimeters(lol);
+        while (scanner.hasNext()) {
+            fileInput += scanner.nextLine();
+        }
 
-		for  (String si : cleanString) {
-			System.out.print("length : " + si.length() + " String: ");
-			System.out.println(si);
-		}
-	}
+        caseSensitive = userConstraint.equals(userOptions[0]) || userConstraint.equals(userOptions[2]) ||
+                userConstraint.equals(userOptions[3]);
+        
+
+        if (args.length == 0 || !caseSensitive) {
+            cleanStringArray = wordFrequencyArray(fileInput);
+        } else {
+            cleanStringArray = wordFrequencyArrayWithoutUppercase(fileInput);          
+        } 
+
+        HashMap mapOfWords = turnIntoHashMapAndSet(cleanStringArray);
+
+        System.out.println(mapOfWords.keySet());
+
+        scanner.close();
+    }
 }
+
+
+
+
